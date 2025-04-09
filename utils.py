b8cdf29fc404a47300ab556a7fb082dd828055da
kamaldeepsagar01 <55277389+kamaldeepsagar01@users.noreply.github.com>
2025-04-09 21:24:22 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -89,7 +89,7 @@ def get_synthetic_user():
             "lon": 77.5946
         },
         "weather": "Clear",
-        "current_time": "Saturday 8 AM",
+        "current_time": "Friday 2 PM",
         #"free_hours": 4,
         "calendar": [
             {"event": "Lunch with friend", "start": "1 PM", "end": "2 PM"},
