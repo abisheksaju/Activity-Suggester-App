13d1f0b6ff17e86f5e60e44fa6d07472e61d255a
kamaldeepsagar01 <55277389+kamaldeepsagar01@users.noreply.github.com>
2025-04-22 09:05:25 +0530

    Update utils.py - Reverted Shopping to Travel as First Pref

--- utils.py
+++ utils.py
@@ -1362,14 +1362,14 @@ def get_user_preferences_db():
         st.session_state.user_preferences = {
             "category_preferences": {
                 "food": 0.2,
-                "travel": 0.5,
-                "event": 0.5,
+                "travel": 0.9,
+                "event": 0.8,
                 "music": 0.7,
                 "gaming": 0.2,
                 "news": 0.2,
                 "fitness": 0.3,
                 "cooking": 0.2,
-                "shopping": 0.9
+                "shopping": 0.6
             },
             "liked_places": [],
             "disliked_places": []
